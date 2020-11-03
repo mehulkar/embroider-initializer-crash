49552558306ebaf0fc55df5e023aa2ce76b0856b
Mehul Kar <mehul.kar@gmail.com>
2020-11-03 10:30:46 -0600

    Build with embroider based on env var

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
-  return app.toTree();
+  if (process.env.BUILD_EMBROIDER === 'true')  {
+    const { Webpack } = require('@embroider/webpack');
+    return require('@embroider/compat').compatBuild(app, Webpack);
+  } else {
+    return app.toTree();
+  }
 };
